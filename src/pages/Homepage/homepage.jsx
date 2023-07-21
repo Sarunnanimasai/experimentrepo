0a6e389ca718cd9ce2f005d88818081a59325261
Sarunnanimasai <103571317+Sarunnanimasai@users.noreply.github.com>
2023-07-21 12:23:13 +0530

    added components and pages

---
+++ src/pages/Homepage/homepage.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Layout from "../../layout/layout";
+import { Carousel } from "@mantine/carousel";
+import { CardsCarousel } from "../../components/Carousel/carousel";
+
+function HomePage() {
+  return (
+    <Layout>
+      <h1>Explore for more Furniture Ideas</h1>
+      <CardsCarousel />
+      <div>
+        <h1>About Us</h1>
+
+        <p style={{ fontWeight: "bold", width: "75%", margin: "auto" }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+    </Layout>
+  );
+}
+
+export default HomePage;
---
+++ src/pages/landingpage/landingpage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../../layout/layout";
+import { CarouselCard } from "../../components/smallcarousel/smallcarousel";
+
+function LandingPage() {
+  return (
+    <Layout>
+      <CarouselCard />
+    </Layout>
+  );
+}
+
+export default LandingPage;
